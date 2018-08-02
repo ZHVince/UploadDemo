469aca3d167f366bd8a5c570c93ccecace425b5b
ZHVince <940088091@qq.com>
2018-08-02 14:11:20 +0800

    增加类
    增加类

---
+++ uploadDemo/view1.h
@@ -0,0 +1,13 @@
+//
+//  view1.h
+//  uploadDemo
+//
+//  Created by 铂金数据 on 2018/8/2.
+//  Copyright © 2018年 张恒. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface view1 : NSObject
+
+@end
---
+++ uploadDemo/view1.m
@@ -0,0 +1,13 @@
+//
+//  view1.m
+//  uploadDemo
+//
+//  Created by 铂金数据 on 2018/8/2.
+//  Copyright © 2018年 张恒. All rights reserved.
+//
+
+#import "view1.h"
+
+@implementation view1
+
+@end
