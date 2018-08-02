b346f510f307991e58910239cbc0820e01abc8b0
ZHVince <940088091@qq.com>
2018-08-02 14:01:23 +0800

    init
    init

---
+++ uploadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  uploadDemo
+//
+//  Created by 铂金数据 on 2018/8/2.
+//  Copyright © 2018年 张恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ uploadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  uploadDemo
+//
+//  Created by 铂金数据 on 2018/8/2.
+//  Copyright © 2018年 张恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
